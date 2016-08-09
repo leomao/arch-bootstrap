59371725a7c26d02c70a65701efcb33b1fba3883
Leo Mao <leomaoyw@gmail.com>
2016-08-09 22:46:39 +0800

    Add some functions

--- base.sh
+++ base.sh
@@ -1,6 +1,23 @@
 #! /bin/sh
 
-packages=(
+function install_packages() {
+  xargs pacman --force --noconfirm -S "$@"
+}
+
+function locale_setting() {
+  sed -e "s/^#en_US\.UTF-8/en_US\.UTF-8/" /etc/locale.gen
+  sed -e "s/^#zh_TW\.UTF-8/zh_TW\.UTF-8/" /etc/locale.gen
+  locale-gen
+  echo "LANG=en_US.UTF-8" > /etc/locale.conf
+}
+
+function time_setting() {
+  timedatectl set-timezone ${TIMEZONE}
+  hwclock --systohc --utc
+  timedatectl set-ntp true
+}
+
+basic_packages=(
   "sudo"
   "ntp"
   "zsh"
@@ -14,16 +31,7 @@ packages=(
   "the_silver_searcher"
 )
 
-sed -e "s/^#en_US\.UTF-8/en_US\.UTF-8/" /etc/locale.gen
-sed -e "s/^#zh_TW\.UTF-8/zh_TW\.UTF-8/" /etc/locale.gen
-locale-gen
 
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-timedatectl set-timezone ${TIMEZONE}
-hwclock --systohc --utc
 hostnamectl set-hostname ${HOSTNAME}
-
 useradd -m -G wheel,storage,power ${USER}
 
-echo ${packages[*]} | xargs pacman --force --noconfirm -S
-timedatectl set-ntp true
